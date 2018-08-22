f9f6b1b40844c9c257e90988d3afb6eac9dc2dd2
thankzero <42596740+thankzero@users.noreply.github.com>
2018-08-22 14:25:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'TIk5FXliqNRSvrKFroXpsfmzDrnjWlOH4OU6JOv8TdATYqK0avIu0XkeeAIlWkU9do6muoraVhZHqFI6H8smwCZgWvByewVW0shcLjsvfQllsFT6wHFPY4Nn88iqg7Vdns3SKa04zYBmWFHIdYg/egdB04t89/1O/w1cDnyilFU=';
+$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -49,3 +49,4 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+
